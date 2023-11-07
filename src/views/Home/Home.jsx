3518cd9552695ed9603d296ea206914cbd09a897
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-11-07 23:50:02 +0530

    added design in card

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Home.css'
 
 import { useSearchParams } from 'react-router-dom';
 const GREATINGS =[
-    "happy dipaval",
-    "dipavali",
+    "Dipavalichy hardic shubhekchy",
+    "Happy dipavali",
    " shub dipavali"
 ]
+
+// localhost:3000/?to=santu&from=abhijeet&g=2
+
+
 function Home() {
     <h1>home</h1>
     const [ searchParams] = useSearchParams();
@@ -14,10 +19,11 @@ function Home() {
     const from = searchParams.get("from");
     const greetingNumber = searchParams.get("g") >= GREATINGS.length ? 0 : searchParams.get("g") || 0 ;
   return (
-    <div>
-<h1>  dear {to} </h1>
+    <div className='greeting-card'>
+      <h1>Dipavali 🤪Greading Card</h1>
+<h3>  dear {to} </h3>
 <p> {GREATINGS[greetingNumber]} </p>
-<h2> {from} </h2>
+<h3 className='from-name'>Shubhekchuk: {from} </h3>
 
     </div>
   )
